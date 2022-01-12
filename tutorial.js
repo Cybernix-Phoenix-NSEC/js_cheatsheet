c1eeb30bcc070feddee137c7d0dabc3e7d148203
Dipikesh kumar <71708235+Dipikesh@users.noreply.github.com>
2022-01-12 21:32:29 +0530

    Create tutorial.js

---
+++ tutorial.js
@@ -0,0 +1,74 @@
+// console.log("Hello world!");
+
+// data type
+
+// let names = "Dipikesh";
+// let num = 2.34;
+
+// let isAuthenticated = true;
+// console.log(typeof names);
+// console.log(typeof num);
+// console.log(typeof isAuthenticated)
+
+// console.log("thhis" + "is" + "good");
+// console.log("3" + "4");
+
+// var names = "Dipikesh";
+// names = "kumar"
+// console.log(names);
+
+
+// const person = "Dipikesh";
+// console.log(person);
+
+
+//Condition Statement
+
+// var numbers = prompt("Enter number");
+// console.log(numbers);
+
+// var num = 4;
+// console.log(num==4);
+// // if(true)
+
+// if (num == 2) {
+//     console.log("Hurray, I won the lottery");
+// }
+
+// else {
+//     console.log("Bad luck")
+// }
+
+// const fruit = {
+//     names: "Apple",
+//     color:"Red"
+// }
+
+// console.log(fruit.color);
+
+
+// var age = 20;
+// var names = "Subhadeep";
+// var profession = "Student";
+
+// var person = {
+//     age: {hello:"okay"},//age:age//age:20
+//     names,//names:names
+//     profession
+// }
+// console.log(person.age.hello);
+
+
+// 0 : 1,
+//     1 : 2,
+//     2: 4,
+//         3: 5
+// var numbers = [1, 2, 4, 5];
+// console.log(numbers[5]);
+
+// function displayName(name) {
+//     console.log(name+" "+"kumar");
+// }
+// var myName = prompt("Enter your name");
+// displayName(myName);
+
